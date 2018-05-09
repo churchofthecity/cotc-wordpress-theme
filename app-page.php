da1ab7d46ce578f222b6db6bc4993a5df5327a56
Jason York <jason.york@gmail.com>
2018-05-09 18:40:09 -0500

    Initial Commit

---
+++ app-page.php
@@ -0,0 +1,155 @@
+<?php
+
+/*
+Template Name: App Page
+*/
+ 
+global $wp_query;
+$id = $wp_query->get_queried_object_id();
+$sidebar = get_post_meta($id, "qode_show-sidebar", true);  
+
+
+if(get_post_meta($id, "qode_page_background_color", true) != ""){
+	$background_color = get_post_meta($id, "qode_page_background_color", true);
+}else{
+	$background_color = "";
+}
+
+$content_style_spacing = "";
+if(get_post_meta($id, "qode_margin_after_title", true) != ""){
+	if(get_post_meta($id, "qode_margin_after_title_mobile", true) == 'yes'){
+		$content_style_spacing = "padding-top:".esc_attr(get_post_meta($id, "qode_margin_after_title", true))."px !important";
+	}else{
+		$content_style_spacing = "padding-top:".esc_attr(get_post_meta($id, "qode_margin_after_title", true))."px";
+	}
+}
+
+if ( get_query_var('paged') ) { $paged = get_query_var('paged'); }
+elseif ( get_query_var('page') ) { $paged = get_query_var('page'); }
+else { $paged = 1; }
+
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="mobile-web-app-capable" content="yes">
+	<?php
+	if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) {
+		echo('<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">');
+	} ?>
+
+	<title><?php wp_title(''); ?></title>
+
+	<?php
+	/**
+	 * qode_header_meta hook
+	 *
+	 * @see qode_header_meta() - hooked with 10
+	 * @see qode_user_scalable_meta() - hooked with 10
+	 */
+	do_action('qode_header_meta');
+	?>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo esc_url($qode_options_proya['favicon_image']); ?>">
+	<link rel="apple-touch-icon" href="<?php echo esc_url($qode_options_proya['favicon_image']); ?>"/>
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+	<?php // get_header(); ?>
+	<?php do_action('qode_header_meta'); ?>
+	<?php // get_template_part( 'title' ); ?>
+		
+		<div class="container"<?php if($background_color != "") { echo " style='background-color:". $background_color ."'";} ?>>
+           
+			<div class="container_inner_app default_template_holder clearfix page_container_inner" <?php qode_inline_style($content_style_spacing); ?>>
+				<?php if(($sidebar == "default")||($sidebar == "")) : ?>
+					<?php if (have_posts()) : 
+							while (have_posts()) : the_post(); ?>
+							<?php the_content(); ?>
+							<?php 
+								$args_pages = array(
+									'before'           => '<p class="single_links_pages">',
+									'after'            => '</p>',
+									'pagelink'         => '<span>%</span>'
+								);
+								wp_link_pages($args_pages);
+							?>
+
+							<?php endwhile; ?>
+						<?php endif; ?>
+				<?php elseif($sidebar == "1" || $sidebar == "2"): ?>		
+					
+					<?php if($sidebar == "1") : ?>	
+						<div class="two_columns_66_33 background_color_sidebar grid2 clearfix">
+							<div class="column1">
+					<?php elseif($sidebar == "2") : ?>	
+						<div class="two_columns_75_25 background_color_sidebar grid2 clearfix">
+							<div class="column1">
+					<?php endif; ?>
+							<?php if (have_posts()) : 
+								while (have_posts()) : the_post(); ?>
+								<div class="column_inner">
+								
+								<?php the_content(); ?>
+								<?php 
+									$args_pages = array(
+									'before'           => '<p class="single_links_pages">',
+									'after'            => '</p>',
+									'pagelink'         => '<span>%</span>'
+									);
+
+									wp_link_pages($args_pages);
+								?>
+
+								</div>
+						<?php endwhile; ?>
+						<?php endif; ?>
+					
+									
+							</div>
+							<div class="column2"><?php get_sidebar();?></div>
+						</div>
+					<?php elseif($sidebar == "3" || $sidebar == "4"): ?>
+						<?php if($sidebar == "3") : ?>	
+							<div class="two_columns_33_66 background_color_sidebar grid2 clearfix">
+								<div class="column1"><?php get_sidebar();?></div>
+								<div class="column2">
+						<?php elseif($sidebar == "4") : ?>	
+							<div class="two_columns_25_75 background_color_sidebar grid2 clearfix">
+								<div class="column1"><?php get_sidebar();?></div>
+								<div class="column2">
+						<?php endif; ?>
+								<?php if (have_posts()) : 
+									while (have_posts()) : the_post(); ?>
+									<div class="column_inner">
+										<?php the_content(); ?>
+										<?php 
+											$args_pages = array(
+												'before'           => '<p class="single_links_pages">',
+												'after'            => '</p>',
+												'pagelink'         => '<span>%</span>'
+											);
+											wp_link_pages($args_pages);
+										?>
+
+									</div>
+							<?php endwhile; ?>
+							<?php endif; ?>
+						
+										
+								</div>
+								
+							</div>
+					<?php endif; ?>
+			
+		</div>
+        <?php if(isset($qode_options_proya['overlapping_content']) && $qode_options_proya['overlapping_content'] == 'yes') {?>
+            </div></div>
+        <?php } ?>
+	</div>
---
+++ functions.php
@@ -0,0 +1,83 @@
+<?php
+
+function remove_lostpassword_text ( $text ) {
+  if ($text == 'Lost your password?'){$text = '';}
+  return $text;
+}
+add_filter( 'gettext', 'remove_lostpassword_text' );
+
+function disable_password_reset() { return false; }
+add_filter ( 'allow_password_reset', 'disable_password_reset' );
+
+
+// enqueue the child theme stylesheet
+
+Function wp_schools_enqueue_scripts() {
+  wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+  wp_enqueue_style( 'childstyle' );
+  wp_enqueue_script("cotc_custom", get_stylesheet_directory_uri() ."/js/cotc_custom.js", array('jquery'), false, true);
+}
+add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
+
+
+// include_once('the-events-calendar-short-code.php');
+include_once( get_stylesheet_directory() . '/the-events-calendar-short-code.php' );
+
+
+// Override the rss event link
+// This causes an error here:
+// http://churchofthecity.com/category/40-days/feed/
+// add_filter( 'the_permalink_rss', 'cotc_the_permalink_rss', 10, 0);
+function cotc_the_permalink_rss() {
+  global $post;
+  if ($post->post_type == 'tribe_events') {
+    $event_url = tribe_get_event_meta( $post->id, '_EventURL', true );
+    if ( ! empty( $event_url ) ) {
+      $parseUrl = parse_url( $event_url );
+      if ( empty( $parseUrl['scheme'] ) ) {
+        $event_url = "http://$event_url";
+      }
+      return $event_url;
+    }
+  } else {
+    return the_permalink_rss();
+  }
+}
+
+// Override the event link
+add_filter( 'tribe_get_event_link', 'cotc_tribe_get_event_link', 10, 4);
+function cotc_tribe_get_event_link($link, $postId, $full_link, $url ) {
+  $event_url = tribe_get_event_meta( $postId, '_EventURL', true );
+  if ( ! empty( $event_url ) ) {
+    $parseUrl = parse_url( $event_url );
+    if ( empty( $parseUrl['scheme'] ) ) {
+      $event_url = "http://$event_url";
+    }
+    return $event_url;
+  } else {
+    return $link;
+  }
+}
+
+// Override the RSS2 feed
+// remove_all_actions( 'do_feed_rss2' );
+add_action( 'do_feed_rss2', 'cotc_feed_rss2', 10, 1 );
+
+function cotc_feed_rss2( $for_comments ) {
+    $rss_template = get_stylesheet_directory() . '/feed-cotc-rss2.php';
+    if( get_query_var( 'post_type' ) == 'tribe_events' and file_exists( $rss_template ) )
+        load_template( $rss_template );
+    else
+        do_feed_rss2( $for_comments ); // Call default function
+}
+
+// Supply the event date in the RSS feed content
+function event_date_in_rss($content) {
+  global $post;
+  $news_only = get_post_custom_values('news_only');
+  if( $post->post_type == 'tribe_events' && !$news_only ) {
+    $content = '<p><em>Starts ' . tribe_get_start_date( $post, false, 'l F jS Y \a\t h:i A' ) . "</em></p>" . $content;
+  }
+  return $content;
+}
+// add_filter('the_content_feed', 'event_date_in_rss');
